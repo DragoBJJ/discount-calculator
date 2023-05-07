d6fc7ee37c132d0ee0b473318ce1138fac8e7e18
DragoBJJ <j.pawelski.it@gmail.com>
2023-05-07 20:53:23 +0200

    update addSeriveToOrderFunction

--- app/calculator/components/Service.tsx
+++ app/calculator/components/Service.tsx
@@ -1,29 +1,28 @@
 "use client"
-import {memo} from "react";
+import {memo, useState} from "react";
 import {MdAdd} from "react-icons/md"
 import c from "../../styles/variables.module.scss"
 import  {useCompanyContext} from "@/app/calculator/context/companyContext";
-import { ServiceTypeF} from "@/app/calculator/types";
+import {RabatType, ServiceTypeF} from "@/app/calculator/types";
 import {showRabatForService} from "@/app/calculator/common/common";
 
 
     export const Service = memo<ServiceTypeF>(({id,name,price}) => {
         const {setCompanyServices,services: orders} = useCompanyContext()
-
-
-        const addService = (id: ServiceTypeF["id"],name: ServiceTypeF["name"],price: ServiceTypeF["price"]) => {
+       const [visibleDiscount, setVisibleDiscounts] = useState<RabatType[] | null>(null)
+    console.log("visibleDiscount",visibleDiscount)
+        const addServiceToOrder = (id: ServiceTypeF["id"],name: ServiceTypeF["name"],price: ServiceTypeF["price"]) => {
              if(orders.some(service => service.id === id )) return
             setCompanyServices([...orders,  {id, name,price}])
-            const success = showRabatForService(id)
+            setVisibleDiscounts(showRabatForService(id))
         }
-
 
             return (
             <div id="service" className={c.service}  >
                 <h4 id="service_title" className={c.title}>{name}</h4>
                 <p className={c.price}>{price} zł</p>
                 <div className={c.buttonsWrapper}>
-                    <MdAdd onClick={()=> addService(id, name,price)} className={c.addButton} size="25px"/>
+                    <MdAdd onClick={()=> addServiceToOrder(id, name,price)} className={c.addButton} size="25px"/>
                 </div>
             </div>
         )
